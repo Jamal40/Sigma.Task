81585c73f2b09aa0af7ad5cc6b69c1083424bf35
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2022-11-18 20:25:36 +0200

    Adding DTOs Project

---
+++ Sigma.Task.DTOs/Candidates/CandidateWriteDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sigma.Task.DTOs;
+
+public record CandidateWriteDTO
+{
+    [EmailAddress]
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string CallTimeInterval { get; set; } = string.Empty;
+
+    [Url]
+    public string LinkedInProfile { get; set; } = string.Empty;
+
+    [Url]
+    public string GitHubProfile { get; set; } = string.Empty;
+
+    [Required]
+    public string Comment { get; set; } = string.Empty;
+}
